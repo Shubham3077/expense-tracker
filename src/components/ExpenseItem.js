92f08dd88148745d69741fbbcd449550b41097b6
Shubham Kumar <rdfsshubham@gmail.com>
2023-01-23 17:49:55 +0530

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExpenseItem () {
-    return <div className='expense-item '>
-        <div > Date - 23 jan , 2023</div>
-        <div className='expense-item__description'>
-            <h2 className= 'expense-item h2'>Title - Book</h2>
-            <div className='expense-item__price'>Amount - ₹40</div>
-        </div>
-    </div>
+function ExpenseItem(props) {
+	return (
+		<div className="expense-item ">
+			<div>{props.date.toISOString()}</div>
+			<div className="expense-item__description">
+				<h2 className="expense-item h2">
+					{props.title}
+				</h2>
+				<div className="expense-item__price">
+					{props.amount}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExpenseItem;
